c37a2ad2a5d7469c88183ddc94ea858f50e72abd
Гладнев Евгений Зурабович <evgeniy.gladnev@rt.ru>
2023-10-30 09:05:26 +0300

    ДЗ исправление_1

--- test.sql
+++ test.sql
@@ -4,6 +4,6 @@ create table questions(question_id SERIAL PRIMARY key, question VARCHAR(255));
 
 create table answers(answer_id SERIAL PRIMARY key, answer VARCHAR(255), correct_answer INT);
 
-create table tests(test_id INT PRIMARY key, test_name VARCHAR(255));
+create table tests(test_id SERIAL PRIMARY key, test_name VARCHAR(255));
 
-create table options(option_id INT PRIMARY key, question_id INT, answer_id INT, test_id INT);
+create table options(option_id SERIAL PRIMARY key, question_id INT, answer_id INT, test_id INT);
